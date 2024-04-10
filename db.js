3224a46acdcff856994b8c1bccaa0a18d515181c
L2240 <L2240@lisboa.epbjc.local>
2024-04-10 16:24:02 +0100

    10/04

--- db.js
+++ db.js
@@ -73,6 +73,46 @@ app.post('/register', async (req, res) => {
     }
 });
 
+app.get('/availability', async (req, res) => {
+    const { startDate, endDate } = req.query; // Use req.query para obter os parâmetros da solicitação GET
+  
+    try {
+        const pool = await mssql.connect(config); // Conecte-se ao banco de dados usando as configurações
+  
+        const query = `
+    SELECT *
+    FROM tipo_quarto
+    WHERE Id_tipo NOT IN (
+        SELECT Id_quarto
+        FROM reservas_quarto
+        WHERE Data_inicio <= @startDate AND Data_fim >= @endDate
+    )
+    AND Lotacao >= (
+        SELECT COUNT(Id_quarto)
+        FROM reservas_quarto
+        WHERE Data_inicio <= @startDate AND Data_fim >= @endDate
+        AND tipo_quarto.Id_tipo = reservas_quarto.Id_quarto
+    )`;
+
+
+      
+        const result = await pool.request()
+            .input('startDate', mssql.Date, startDate)
+            .input('endDate', mssql.Date, endDate)
+            .query(query);
+  
+        const availableRooms = result.recordset; // Use result.recordset para obter os resultados da consulta
+  
+        res.json({ availableRooms: availableRooms });
+    } catch (error) {
+        console.error('Erro ao verificar disponibilidade:', error);
+        res.status(500).json({ error: 'Erro ao verificar disponibilidade' });
+    }
+});
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- src/components/paginas/reservas.js
+++ src/components/paginas/reservas.js
@@ -10,15 +10,15 @@ function Reservas() {
   const [selectedRooms, setSelectedRooms] = useState({});
   const [totalPrice, setTotalPrice] = useState(0);
   const [warning, setWarning] = useState(false);
-  const [price,setPrice] = useState(false);
+  const [price, setPrice] = useState(false);
   const [availableRooms, setAvailableRooms] = useState([]);
-  
+
   const cards = [
-    { id: 1, title: "Suite", value: 100, area: "50m²", capacity: "2 Pessoas", bedType: "Cama King Size", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
-    { id: 2, title: "Quarto Duplo", value: 150, area: "25m²", capacity: "2 Pessoas", bedType: "Cama de Casal", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
-    { id: 3, title: "Quarto Twin", value: 200, area: "30m²", capacity: "2 Pessoas", bedType: "2 Camas Individuais", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
-    { id: 4, title: "Suite Familiar", value: 250, area: "60m²", capacity: "4 Pessoas", bedType: "Cama King Size e 2 Camas de Solteiro", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
-    { id: 5, title: "Quarto Solteiro", value: 120, area: "18m²", capacity: "1 Pessoa", bedType: "Cama de Solteiro", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] }
+    { id: 1, title: "Suite", value: 625, area: "50m²", capacity: "2 Pessoas", bedType: "Cama King Size", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
+    { id: 2, title: "Quarto Duplo", value: 390, area: "25m²", capacity: "2 Pessoas", bedType: "Cama de Casal", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
+    { id: 3, title: "Quarto Twin", value: 450, area: "30m²", capacity: "2 Pessoas", bedType: "2 Camas Individuais", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
+    { id: 4, title: "Suite Familiar", value: 600, area: "60m²", capacity: "4 Pessoas", bedType: "Cama King Size e 2 Camas de Solteiro", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] },
+    { id: 5, title: "Quarto Solteiro", value: 300, area: "18m²", capacity: "1 Pessoa", bedType: "Cama de Solteiro", amenities: ["Ar condicionado", "Pequeno-Almoço incluído", "Parque gratuito", "TV LCD", "Internet Wi-Fi"] }
   ];
 
   const handleReserve = (roomId, value) => {
@@ -33,15 +33,31 @@ function Reservas() {
     } else {
       setWarning(true);
     }
-    if(totalRoomsCount+value==0)
-    setPrice(false);
+    if (totalRoomsCount + value === 0)
+      setPrice(false);
   };
 
-  const handleAvailabilityCheck = () => {
-    // Simular uma verificação de disponibilidade na base de dados com os IDs selecionados e a data escolhida
-    // Aqui você pode implementar a lógica para verificar a disponibilidade dos quartos
-    const available = cards.filter(card => selectedRooms[card.id] > 0);
-    setAvailableRooms(available);
+  const Availability = async () => {
+    try {
+      
+      const formattedStartDate = startDate.toISOString().split('T')[0];
+      const formattedEndDate = endDate.toISOString().split('T')[0];
+      const response = await fetch(`http://localhost:4000/availability?startDate=${formattedStartDate}&endDate=${formattedEndDate}`);
+
+
+      if (!response.ok) {
+        throw new Error('Erro ao verificar disponibilidade');
+      }
+
+      const data = await response.json();
+      if (data && data.availableRooms) {
+        setAvailableRooms(data.availableRooms);
+      } else {
+        setAvailableRooms([]);
+      }
+    } catch (error) {
+      console.error('Erro', error);
+    }
   };
 
   const updateTotalPrice = (updatedRooms) => {
@@ -92,53 +108,47 @@ function Reservas() {
             </div>
           </td>
           <td >
-            <button type="button" >Verificar disponibilidade</button>
+            <button type="button" className="verificarbutton" onClick={Availability} >Verificar disponibilidade</button>
           </td>
         </table>
       </div>
 
       <div className="reservas-cards">
-        {cards.map(card => (
-          <div className="card" key={card.id}>
-            <div className="card-image">
-              <img src={`../Imagens/${card.title.replace(/\s+/g, "_").toLowerCase()}--/${card.title.replace(/\s+/g, "").toLowerCase()}1.jpg`} alt={card.title} />
-            </div>
-            <div className="card-content">
-              <p className="roomtitle">{card.title}</p>
-              <p><strong>Área do quarto:</strong> {card.area}</p>
-              <p><strong>Capacidade:</strong> {card.capacity}</p>
-              <p><strong>Tipo de cama:</strong> {card.bedType}</p>
-              <p><strong>Comodidades:</strong></p>
-              <ul>
-                {card.amenities.map((amenity, index) => (
-                  <li key={index}>{amenity}</li>
-                ))}
-              </ul>
-              <p className="pricetag"><strong>Preço por noite: </strong> {card.value}€</p>
-              <div className="card-buttons">
-                {selectedRooms[card.id] === undefined || selectedRooms[card.id] === 0 ? (
-                  <button className="button1" onClick={() => handleReserve(card.id, 1)}>
-                    Reservar
-                  </button>
-                ) : (
-                  <div className="contador">
-                    
-                          <button  onClick={() => handleIncrease(card.id)}>
-                            +
-                          </button>
-                       
-                          <span >{selectedRooms[card.id]}</span>
-                       
-                          <button  onClick={() => handleDecrease(card.id)}>
-                            -
-                          </button>
-                        
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
+      {availableRooms && availableRooms.length > 0 && availableRooms.map(room => (
+  <div className="card" key={room.id}>
+    <div className="card-image">
+      {room && room.title && (
+        <img src={`../Imagens/${room.title.replace(/\s+/g, "_").toLowerCase()}--/${room.title.replace(/\s+/g, "").toLowerCase()}1.jpg`} alt={room.title} />
+      )}
+    </div>
+    <div className="card-content">
+      <p className="roomtitle">{room.title}</p>
+      <p><strong>Área do quarto:</strong> {room.area}</p>
+      <p><strong>Capacidade:</strong> {room.capacity}</p>
+      <p><strong>Tipo de cama:</strong> {room.bedType}</p>
+      <p><strong>Comodidades:</strong></p>
+      <ul>
+        {room.amenities.map((amenity, index) => (
+          <li key={index}>{amenity}</li>
         ))}
+      </ul>
+      <p className="pricetag"><strong>Preço por noite: </strong> {room.value}€</p>
+      <div className="card-buttons">
+        {selectedRooms[room.id] === undefined || selectedRooms[room.id] === 0 ? (
+          <button className="button1" onClick={() => handleReserve(room.id, 1)}>
+            Reservar
+          </button>
+        ) : (
+          <div className="contador">
+            <button onClick={() => handleIncrease(room.id)}>+</button>
+            <span>{selectedRooms[room.id]}</span>
+            <button onClick={() => handleDecrease(room.id)}>-</button>
+          </div>
+        )}
+      </div>
+    </div>
+  </div>
+))}
       </div>
 
       {warning && (
@@ -147,19 +157,20 @@ function Reservas() {
         </div>
       )}
 
-      {price &&(
-      <div className="tray">
-        <table>
-          <tr>
-            <td>
-            <h2>Total Preço: {totalPrice}€</h2>
-            </td>
-            <td>
-            <button className="reservarbtn">Reservar</button>
-            </td>
-          </tr>
-        </table>
-      </div>)}
+      {price && (
+        <div className="tray">
+          <table>
+            <tr>
+              <td>
+                <h2>Total Preço: {totalPrice}€</h2>
+              </td>
+              <td>
+                <button className="reservarbtn">Reservar</button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      )}
       
     </div>
   );
