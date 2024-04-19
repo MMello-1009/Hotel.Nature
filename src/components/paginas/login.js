f3a7b5d4239b23a65e203bd9e0cfe47d4230c4ef
L2240 <L2240@lisboa.epbjc.local>
2024-04-19 11:26:00 +0100

    19/04

--- src/components/paginas/login.js
+++ src/components/paginas/login.js
@@ -106,7 +106,7 @@ const LoginPopup = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-                <button className="login-submit" type="submit">
+                <button className="loginsubmit" type="submit">
                   Login
                 </button>
                 <button className="register-toggle" type="button" onClick={toggleRegister}>
